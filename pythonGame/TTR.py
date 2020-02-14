09ee814124cbfdeaafd84495755bb16014b19f64
Jordan <koromjm19@uww.edu>
2020-02-13 21:54:43 -0600

    added 2D edge array and array of city names, prints the edges color in console. No longer prints you have over 14 cards when clicking on the background

--- pythonGame/TTR.py
+++ pythonGame/TTR.py
@@ -46,6 +46,52 @@ class Card:
     def setColor(self, color):
         self.color = color
 
+class Edge:
+
+    def __init__(self, length, connection, color):
+        self.length = length
+        self.occupied = False
+        self.connection = connection
+        self.color = color
+
+    def getLength(self):
+        return self.length
+
+    def setLength(self, length):
+        self.length = length
+
+    def getColor(self):
+        return self.color
+
+    def setColor(self, color):
+        self.occupied = color
+
+    def getConnection(self):
+        return self.connection
+
+    def setConnection(self, connection):
+        self.connection = connection
+
+##numNodes = input('How many cities?')
+
+cityConnection = ([[-1,                      Edge(3, [0, 1], 'black'), -1,                      Edge(5, [0, 3],'white'),  Edge(2, [0, 4],'black'),  -1,                      -1],
+                   [Edge(3, [1, 0],'black'), -1,                       Edge(4, [1, 2],'white'), -1,                       -1,                       -1,                      -1],
+                   [-1,                      Edge(4, [2, 1],'white'),  -1,                      Edge(6, [2, 3],'black'),  -1,                       Edge(4, [2, 5],'black'), -1],
+                   [Edge(5, [3, 0],'white'), -1,                       Edge(6, [3, 2],'black'), -1,                       -1,                       -1,                      Edge(3, [3, 6],'white')],
+                   [Edge(2, [4, 0],'black'), -1,                        -1,                     -1,                       -1,                       Edge(3, [4, 5],'white'), Edge(3, [4, 6],'white')],
+                   [-1,                      -1,                       Edge(4, [5, 2],'black'), -1,                       Edge(3,  [5, 4],'white'), -1,                      Edge(2, [5, 6],'black')],
+                   [-1,                      -1,                        -1,                     Edge(3,  [6, 3],'white'), Edge(3,  [6, 4],'white'), Edge(2, [6, 5],'black'), -1]])
+
+cityNames = ['Los Angeles', 'Seattle', 'New York', 'Dallas', 'Salt Lake', 'Milwaukee', 'Chicago']
+
+for row in cityConnection:
+    for colm in row:
+        if type(colm) != int:
+            print(colm.getColor(), end =", ")
+        else:
+            print("none", end =", ")
+    print("\n")
+
 
 ##initilized the hand array and keeps track of the card index
 handCards = []
@@ -60,21 +106,21 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
+            if blackDeck.collidepoint(pos) or whiteDeck.collidepoint(pos):
+                if len(handCards) >= 14:
+                    print('There are 14 cards in your hand, you can not draw any more!')
 
-            if len(handCards) >= 14:
-                print('There are 14 cards in your hand, you can not draw any more!')
+                elif blackDeck.collidepoint(pos):
+                    handCards.append(Card('black'))
+                    print('added black card to your hand')
+                    screen.blit(whiteTrainImg, (display_width * 0.05 + (50 * cardIndex), display_height * 0.05))
+                    cardIndex += 1
 
-            elif blackDeck.collidepoint(pos):
-                handCards.append(Card('black'))
-                print('added black card to your hand')
-                screen.blit(whiteTrainImg, (display_width * 0.05 + (50 * cardIndex), display_height * 0.05))
-                cardIndex += 1
-
-            elif whiteDeck.collidepoint(pos):
-                print('added white card to your hand')
-                handCards.append(Card('white'))
-                screen.blit(blackTrainImg, (display_width * 0.05 + (50 * cardIndex), display_height * 0.05))
-                cardIndex += 1
+                elif whiteDeck.collidepoint(pos):
+                    print('added white card to your hand')
+                    handCards.append(Card('white'))
+                    screen.blit(blackTrainImg, (display_width * 0.05 + (50 * cardIndex), display_height * 0.05))
+                    cardIndex += 1
 
     pygame.display.update()
     clock.tick(10)
